b77b7c51a47279c978f2df81b7d748895a9d3542
Vladyslav Hurzhyi <80459886+vladyslavhurzhyi@users.noreply.github.com>
2022-09-17 20:02:33 +0200

    Add files via upload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const pictures = galleryItems.forEach(({ preview, original, description }) => {
+    const markup = /*html*/ ` <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+
+    galleryRef.insertAdjacentHTML('beforeend', markup);
+});
+
+galleryRef.addEventListener('click', clickOnImg);
+
+function clickOnImg(event) {
+    event.preventDefault();
+
+    const origImage = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+        <img src="${origImage}" width="800" height="600">
+    `);
+
+    instance.show();
+
+    const visible = basicLightbox.visible();
+    if (visible === true) {
+        galleryRef.addEventListener('keydown', (evt) => {
+            if (evt.code === 'Escape') {
+                instance.close();
+            }
+        });
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const pictures = galleryItems.forEach(({ preview, original, description }) => {
+    const markup = /*html*/ ` <li class="gallery_item"><a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a></li>`;
+
+    galleryRef.insertAdjacentHTML('beforeend', markup);
+});
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250,
+});
